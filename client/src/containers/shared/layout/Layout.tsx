d0de0632b748e3bdfa83b21104b47b76e765fd8b
Gabriel Siedler <gabrielsiedler@gmail.com>
2019-10-12 00:29:55 -0400

    removing layout from pending

--- client/src/containers/shared/layout/Layout.tsx
+++ client/src/containers/shared/layout/Layout.tsx
@@ -6,6 +6,10 @@ export class Layout extends React.Component<any, any> {
   render() {
     const { children } = this.props
 
+    if (/pending/.test(window.location.pathname)) {
+      return children
+    }
+
     return (
       <div className="layout">
         <Menu />
