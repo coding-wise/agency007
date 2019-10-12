7ec1bc1b27159118f525edf3ef380820a822384c
Bruno Leonardo Michels <brunolm7@gmail.com>
2019-10-12 01:07:03 -0400

    Add pivotal routes

--- api/src/api/routes.ts
+++ api/src/api/routes.ts
@@ -4,6 +4,7 @@ import asyncHandler from 'express-async-handler'
 import { ensureAuthenticated } from '../middlewares/ensure-authenticated'
 import { authenticate } from './authentication/authenticate'
 import { me } from './me'
+import { pivotal } from './pivotal'
 import { projects } from './projects'
 
 const router = express.Router()
@@ -11,6 +12,7 @@ const router = express.Router()
 router.use('/auth', asyncHandler(authenticate))
 router.use(me)
 router.use(projects)
+router.use(pivotal)
 router.use('/', asyncHandler(ensureAuthenticated), (_, res) => res.send('AUTHENTICATED').status(200))
 router.use(errorMiddleware)
 
