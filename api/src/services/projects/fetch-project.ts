3ddc1cd8015f8af0eda91de23f034b6c240735a1
Bruno Leonardo Michels <brunolm7@gmail.com>
2019-10-12 08:20:04 -0400

    Add pivotal member list on projects api response

--- api/src/services/projects/fetch-project.ts
+++ api/src/services/projects/fetch-project.ts
@@ -1,5 +1,5 @@
 import { db } from '../knex-connection'
-import { getProjectMembers } from '../pivotal'
+import { getAccountMemberships, getProjectMembers } from '../pivotal'
 
 export const fetchProject = async (id: number) => {
   const project = await db('projects')
@@ -11,9 +11,24 @@ export const fetchProject = async (id: number) => {
   if (project.pivotal) {
     project.members.pivotal = []
 
+    // assuming pivotal can only contain a single id
     for (const pivotalProjectId of project.pivotal) {
-      project.members.pivotal.push(...(await getProjectMembers(pivotalProjectId)))
+      project.members.pivotal.push(...(await getAccountMemberships()))
+
+      const pivotalProjectMembers = await getProjectMembers(pivotalProjectId)
+
+      for (const pivotalProjectMember of pivotalProjectMembers) {
+        const index = project.members.pivotal.findIndex(
+          (membership) => membership.person.id === pivotalProjectMember.person.id,
+        )
+
+        if (index !== -1) {
+          project.members.pivotal[index] = { ...pivotalProjectMember, selected: true }
+        }
+      }
     }
+
+    project.members.pivotal.sort((m1, m2) => m1.person.name.localeCompare(m2.person.name))
   }
 
   return project
--- client/src/containers/projects/ProjectMembersEdit.tsx
+++ client/src/containers/projects/ProjectMembersEdit.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { RouteComponentProps } from 'react-router'
+import Select from 'react-select'
 import { bindActionCreators } from 'redux'
 import { getProjectAction } from '../../redux/projects'
 import { Loader } from '../shared/loader/Loader'
@@ -33,6 +34,21 @@ export class ProjectMemberEditComponent extends React.Component<Dispatchers & St
         <h2>Adding members to {project.name} project</h2>
         <div>
           <h3>Pivotal</h3>
+
+          <Select
+            isMulti={true}
+            options={project.members.pivotal.map((membership) => ({
+              value: membership.id,
+              label: membership.person.email,
+            }))}
+            value={project.members.pivotal
+              .filter((membership) => membership.selected)
+              .map((membership) => ({
+                value: membership.id,
+                label: membership.person.email,
+              }))}
+          />
+
           <ul>
             {project.members.pivotal.map((membership) => (
               <li key={membership.id}>{membership.person.email}</li>
