791de2294c086bd6631f4ccdae543dcbb555855e
Gabriel Siedler <gabrielsiedler@gmail.com>
2019-10-13 14:44:57 -0400

    removing bot from return

--- api/src/services/pivotal/get-account-memberships.ts
+++ api/src/services/pivotal/get-account-memberships.ts
@@ -4,5 +4,7 @@ import { provider } from './provider'
 export const getAccountMemberships = async () => {
   const { data } = await provider.get(`accounts/${config.integrations.pivotal.accountId}/memberships`)
 
-  return data
+  const withoutAgencyBot = (data || []).filter((member) => member.person.username !== 'agency007')
+
+  return withoutAgencyBot
 }
